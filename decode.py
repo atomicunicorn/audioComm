16d58c32a47d94e4e6a793ebaa601b659f99cd96
Zachary Gottesman <zig@zacharys-mbp-2.casa-z.org>
2015-01-09 17:28:14 -0800

    decode.py now works if each ascii-freq is given 2048 samples. 1024 has some incorrect encoding happening. added test.py

---
+++ decode.py
@@ -0,0 +1,102 @@
+import numpy as np
+from scipy.io import wavfile
+import sys
+
+print " "
+if len(sys.argv) != 3:
+	print "decode.py takes exactly 3 arguments."
+	sys.exit(0)
+
+samps_per_freq = int(sys.argv[2])
+rate, data = wavfile.read(sys.argv[1])
+dataLength = np.size(data) #audio should be mono
+num_characters = float(dataLength) / float(samps_per_freq)
+freq_step_size = 9700.0 / 98.0
+allowed_error = 1.0 # allowed error in hertz
+
+print "dataLength", dataLength
+print "samps_per_freq: ", samps_per_freq
+print "num_characters: ", num_characters
+print "dataLength / num_characters", dataLength / num_characters
+print "rate: ", rate
+print " "
+
+# given a clip of the wave file (corresponding to one ascii character),
+# get_freq will return the frequency of the sinusoid
+def get_freq(clip):
+	if len(clip) != samps_per_freq:
+		print "clip size not correct for get_freq() function!"
+		return None
+	fft_window = np.fft.rfft(clip)
+	fft_window = np.absolute(fft_window)
+	freq_bins = np.fft.rfftfreq(samps_per_freq, 1.0/float(rate))
+	max_amp = float('-inf')
+	max_freq_ind = 0
+	i = 0
+	while i <= samps_per_freq / 2:
+		if fft_window[i] > max_amp:
+			max_freq_ind = i
+			max_amp = fft_window[i]
+		i += 1
+	max_freq = (float(max_freq_ind) / float(np.size(freq_bins))) * float(rate)
+	#print "Maximum frequency is: ", max_freq
+	return max_freq
+
+# 0th element holds the freq corresponding to the 0th ascii character
+# nth element holds the freq corresponding to the nth ascii character
+def build_freq_array():
+	fft_window = np.zeros(samps_per_freq)
+	freq_array = []
+	i = 0
+	while i < num_characters:
+		start = i * samps_per_freq
+		end = (i * samps_per_freq) + samps_per_freq 
+		freq = get_freq(data[start: end])
+		freq_array.append(freq)
+		i += 1
+	return freq_array
+
+# takes the calculated frequency, and returns a the closest frequency value out of the accepted 
+# frequency values that are used for encoding (i.e. nearest_freq(301.37) == 300)
+def nearest_freq(calculated_freq):
+	temp_freq = calculated_freq - 300.0
+	step_estimate = temp_freq / freq_step_size
+	steps = round(step_estimate) # probably ok, but some limitations on the round function. see pydocs
+	temp_freq = (freq_step_size * steps) + 300.0
+	return temp_freq
+
+def freq_to_ascii(freq):
+	num_steps = (freq - 300.0) / freq_step_size
+	num_steps = round(num_steps)
+	if num_steps < 3:
+		if num_steps == 0:
+			ascii_val = 9
+		if num_steps == 1:
+			ascii_val = 10
+		if num_steps == 2:
+			ascii_val = 11
+		return ascii_val
+	ascii_val = 32 + num_steps - 3
+	return ascii_val
+
+def freq_array_to_ascii_array(freq_array):
+	ascii = []
+	for elem in freq_array:
+		ascii.append(freq_to_ascii(elem))
+	return ascii
+
+def write_message(ascii_array):
+	f = open("message.txt", "w")
+	full_string = ""
+	for elem in ascii_array:
+		full_string += str(unichr(int(elem)))
+	f.write(full_string)
+
+test_array = build_freq_array()
+i = 0
+final_freq_array = []
+while i < len(test_array):
+	final_freq_array.append(nearest_freq(test_array[i]))
+	i += 1
+ascii_array = freq_array_to_ascii_array(final_freq_array)
+write_message(ascii_array)
---
+++ test.py
@@ -0,0 +1,61 @@
+import sys
+import os
+
+def tester(orig_txt_file, decod_txt_file):
+	fo = open(orig_txt_file, "r")
+	fd = open(decod_txt_file, "r")
+	o_text = fo.read()
+	d_text = fd.read()
+	if len(o_text) != len(d_text):
+		print "text files not of same size"
+		print "original length: ", len(o_text)
+		print "decoded length: ", len(d_text)
+		return False
+	i = 0 
+	while i < len(o_text):
+		if o_text[i] != d_text[i]:
+			print "incorrect ascii at index {0}".format(i)
+			return False
+		i += 1
+	print "all tests passed"
+	return True
+
+#creates a .txt file holding the string, also encodes the .txt file as a wav
+# then tests the decoding of the file
+def create_and_test(string, textfile):
+	f = open(textfile, "w")
+	f.write(string)
+	f.close()
+	fileroot = textfile[0:len(textfile) - 3]
+	wavfile = fileroot + "wav"
+	print "textfile is: ", textfile
+	print "wavfile is: ", wavfile
+	os.system("./encode {0} {1}".format(textfile, wavfile))
+	os.system("python decode.py {0} {1}".format(wavfile, 2048))
+	tester(textfile, "message.txt") 
+	os.system("rm {0}*".format(fileroot))
+
+
+def main():
+	create_and_test("I shat myself", "shit.txt")
+	create_and_test("", "empty.txt")
+	create_and_test("""
+
+
+
+		""", "newline.txt")
+	create_and_test("""-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: 2.6.i
+
+mQCNAi+UeBsAAAEEAMP0kXU75GQdzwwlMiwZBUKFUDRgR4wH9y5NP9JaZfVX8shT
+ESbCTbGSEExB2ktEPh5//dkfftsKNlzlAugKFKTgBv20tQ9lDKfdbPVR0HmTLz0e
+wVIeqEue4+Mt/Kq7kMcQy+5sX2RBAiZTYl0n/JdY/WxflU0taq1kH/UUPkklAAUR
+tB5NYXJ0eSBNY0ZseSA8bWFydHlAZnV0dXJlLmNvbT6JAJUCBRAvlHhGrWQf9RQ+
+SSUBAQX+BACnhx7OTb1SfAcJVF/1kuRPUWuGcl57eZgv4syc1O9T3YNr0terWQBT
+K0vFR00FdaBv9X9XwlxaBJHGeiBcmhaiOTwB912ysoteUzZHne3sHPw3MkYboAFx
+xHg43Cnj60OeZG2PKp/kU91ipOJP1cs8/xYOGkeoAMqDfwPeFlkBiA==
+=ddBN
+-----END PGP PUBLIC KEY BLOCK-----
+%""", "pgp.txt")
+
+main()
